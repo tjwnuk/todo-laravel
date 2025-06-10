453f116147faafa87ad2b0531fb45b7a1da5a7f6
anon <tjwnuk@proton.me>
2025-06-10 03:50:59 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile routes - accessible only to authenticated users
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Todo routes - accessible only to authenticated users
+    Route::get('/todo', [TodoController::class, 'index']);
+    Route::get('/todo/sort/priority', [TodoController::class, 'sortByPriority']);
+    Route::get('/todo/sort/deadline', [TodoController::class, 'sortByDeadline']);
+    Route::get('/todo/sort/status', [TodoController::class, 'sortByStatus']);
 });
 
 require __DIR__.'/auth.php';
